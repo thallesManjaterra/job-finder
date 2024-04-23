e84e6c166255109f3376a9310e869cc54bb3ffac
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2024-04-23 15:47:24 -0300

    conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connection')
 
 const PORT = 3000
 
@@ -7,6 +8,16 @@ app.listen(PORT, () => {
   console.log(`O Express está rodando na porta ${PORT}`)
 })
 
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log('Conectou banco com succeeso')
+  })
+  .catch((err) => {
+    console.log('Ocorreu um erro ao connectar', err)
+  })
+
+// routes
 app.get('/', (req, res) => {
   res.send('Está funcionando')
 })
