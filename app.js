784a8a10ca740988dc4857bce7c838c88264b637
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2024-04-23 16:30:43 -0300

    criando rota para adicionar jobs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db/connection')
+const bodyParser = require('body-parser')
 
 const PORT = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.listen(PORT, () => {
   console.log(`O Express está rodando na porta ${PORT}`)
@@ -21,3 +24,6 @@ db.authenticate()
 app.get('/', (req, res) => {
   res.send('Está funcionando')
 })
+
+// job routes
+app.use('/jobs', require('./routes/jobs'))
---
+++ routes/jobs.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const Job = require('../models/Job')
+
+// add job via post
+router.post('/add', (req, res) => {
+  const { title, salary, company, description, email, new_job } = req.body
+
+  // insert
+  Job.create({
+    title,
+    salary,
+    company,
+    description,
+    email,
+    new_job,
+  })
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err))
+})
+
+module.exports = router
