fd383504fa7b63473a52d3094c6c357af5744d5a
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2024-04-24 19:46:33 -0300

    criando view da job individual

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,6 +2,22 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
 
+// detalhe da vaga
+router.get('/view/:id', (req, res) => {
+  Job.findOne({
+    where: { id: req.params.id },
+  })
+    .then((job) => {
+      res.render('view', {
+        job,
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+// form da rota de envio
 router.get('/add', (req, res) => {
   res.render('add')
 })
